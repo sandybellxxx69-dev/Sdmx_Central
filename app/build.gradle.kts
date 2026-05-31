46aae784a545002bb75e9cc6015506d98da80eed
sandybellxxx69-dev <sandybellxxx69@gmail.com>
2026-05-30 23:12:38 -0600

    Update build.gradle.kts
    error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
+  compileSdk { version = release(35) { minorApiLevel = 1 } }
 
   defaultConfig {
     applicationId = "com.aistudio.sdmxpanel.hjklmn"
